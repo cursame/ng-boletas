42c043e3246f43fb629ebc0825db70b33e1d3041
Carlos Cessa <carlos@pixative.com>
2016-07-01 17:35:02 -0500

    Login View i18n
    - Added support to use i18n strings in the application
    - Defined english translation file
    - Modified login template to use translated strings

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import { TranslateService } from 'ng2-translate/ng2-translate';
 
 @Component({
     directives  : [ ROUTER_DIRECTIVES ],
@@ -8,4 +9,13 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
         <router-outlet></router-outlet>
     `
 })
-export class AppComponent {}
+export class AppComponent {
+
+    constructor( public translate : TranslateService ) {
+        // use navigator lang if available
+        var userLang    = navigator.language.split('-')[0];
+        userLang        = /(es|en)/gi.test(userLang) ? userLang : 'en';
+
+        translate.use( userLang );
+    }
+}
--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,19 @@
+import { PLATFORM_PIPES } from '@angular/core';
 import { bootstrap } from '@angular/platform-browser-dynamic';
+import { HTTP_PROVIDERS } from '@angular/http';
+import { TranslatePipe, TRANSLATE_PROVIDERS } from 'ng2-translate/ng2-translate';
 
 import { AppComponent } from './app.component';
 
 import { SESSIONS_ROUTER_PROVIDER } from './sessions/sessions.routes';
 
 bootstrap( AppComponent, [
-        SESSIONS_ROUTER_PROVIDER
+        HTTP_PROVIDERS,
+        TRANSLATE_PROVIDERS,
+        SESSIONS_ROUTER_PROVIDER,
+        {
+            provide     : PLATFORM_PIPES,
+            useValue    : [ TranslatePipe ],
+            multi       : true
+        }
     ]);
