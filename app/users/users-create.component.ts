1958909b56aa2754e4ea710778062eaf7067e8e1
Carlos Cessa <carlos@pixative.com>
2016-07-13 14:05:34 -0500

    User Create Form
    - Added form to the user's create view to input the values for an user
    record
    - Wired the entered data to an user object in the application
    - Included styles and resources for the added UI

--- app/users/users-create.component.ts
+++ app/users/users-create.component.ts
@@ -5,9 +5,20 @@ import {
     ROUTER_DIRECTIVES
 } from '@angular/router';
 
+import {
+    User
+} from './users.module';
+
 @Component({
     directives  : [ ROUTER_DIRECTIVES ],
     selector    : 'users-create',
     templateUrl : 'views/users/create.html'
 })
-export class UsersCreateComponent {}
+export class UsersCreateComponent {
+
+    user : User     = new User();
+
+    public create() {
+        
+    }
+}
