ba0319d4fb4cf44f3107f36dbc35e6fa5936bbec
Carlos Cessa <carlos@pixative.com>
2016-07-13 13:19:07 -0500

    Users Value Object
    - Defined user value object

---
+++ app/users/user.ts
@@ -0,0 +1,4 @@
+export class User {
+
+    constructor( public name : string = '', public email : string = '', public pass : string = '', public school : string = '', public type : number = -1 ) {}
+}
--- app/users/users.module.ts
+++ app/users/users.module.ts
@@ -1,2 +1,3 @@
+export { User } from './user';
 export { UsersCreateComponent } from './users-create.component';
 export { UsersListComponent } from './users-list.component';
