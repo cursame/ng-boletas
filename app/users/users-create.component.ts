85d94f0b3963b263dddaeb151f4166bc7bc03472
Carlos Cessa <carlos@pixative.com>
2016-07-13 18:39:42 -0500

    Users Creation
    - Modified users create component to load a list of schools from the
    backend to assign the user to

--- app/users/users-create.component.ts
+++ app/users/users-create.component.ts
@@ -6,17 +6,29 @@ import {
 } from '@angular/router';
 
 import {
+    School,
+    SchoolsService
+} from '../schools/schools.module';
+import {
     User
 } from './users.module';
 
 @Component({
     directives  : [ ROUTER_DIRECTIVES ],
+    providers   : [ SchoolsService ],
     selector    : 'users-create',
     templateUrl : 'views/users/create.html'
 })
 export class UsersCreateComponent {
 
-    user : User     = new User();
+    user : User         = new User();
+
+    schools : School[]  = [];
+
+    constructor( private _schoolsService : SchoolsService ) {
+        this._schoolsService.query()
+            .then( schools => this.schools = schools );
+    }
 
     public create() {
         
