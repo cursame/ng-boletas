58a4a93becc7571feb942e22d9f65bc988b7f48d
Carlos Cessa <carlos@pixative.com>
2016-07-14 01:18:40 -0500

    School Settings
    - Created settings object for the school value object

--- app/schools/school.ts
+++ app/schools/school.ts
@@ -1,4 +1,8 @@
+import {
+    Settings
+} from './schools.module';
+
 export class School {
 
-    constructor( public _id : string, public name : string ) {}
+    constructor( public _id : string = '', public name : string = '', public settings : Settings = new Settings() ) {}
 }
