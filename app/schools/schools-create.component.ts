80e8005ffd4543be285c9081206cf914c5b6f449
Carlos Cessa <carlos@pixative.com>
2016-07-14 01:27:12 -0500

    Schools Creation
    - Added support to create a school record in the system
    - Included resources for the new functionality

--- app/schools/schools-create.component.ts
+++ app/schools/schools-create.component.ts
@@ -2,15 +2,24 @@ import {
     Component
 } from '@angular/core';
 import {
+    Router,
     ROUTER_DIRECTIVES
 } from '@angular/router';
 
 import {
-    School
+    TranslateService
+} from 'ng2-translate/ng2-translate';
+
+import {
+    School,
+    SchoolsService
 } from './schools.module';
+
+declare var swal : any;
 
 @Component({
     directives  : [ ROUTER_DIRECTIVES ],
+    providers   : [ SchoolsService ],
     selector    : 'schools-create',
     templateUrl : 'views/schools/create.html'
 })
@@ -18,7 +27,16 @@ export class SchoolsCreateComponent {
 
     school : School     = new School();
 
+    constructor( private _router : Router, private _schools : SchoolsService, private _translate : TranslateService ) {}
+
     public create() {
-        
+        this._schools.create( this.school )
+            .then( school => {
+                this._router.navigate([ '/schools/list' ]);
+                swal( this._translate.instant( 'title.schools_creation' ), this._translate.instant( 'message.school_created' ), 'success' );
+            })
+            .catch( error => {
+                swal( this._translate.instant( 'title.schools_creation' ), this._translate.instant( 'message.school_create_error' ), 'error' );
+            });
     }
 }
