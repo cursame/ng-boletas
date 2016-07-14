3ad93f116a61d05a7703a72a92f2986a8755d9c5
Carlos Cessa <carlos@pixative.com>
2016-07-13 20:20:59 -0500

    Users List
    - Added support to display a list of users in the application `users
    list` component
    - Included styles and resources for the added UI

--- app/users/users-list.component.ts
+++ app/users/users-list.component.ts
@@ -5,9 +5,27 @@ import {
     ROUTER_DIRECTIVES
 } from '@angular/router';
 
+import {
+    User,
+    UsersService
+} from './users.module';
+
 @Component({
     directives  : [ ROUTER_DIRECTIVES ],
+    providers   : [ UsersService ],
     selector    : 'users-list',
     templateUrl : 'views/users/list.html'
 })
-export class UsersListComponent {}
+export class UsersListComponent {
+
+    users : User[]  = [];
+
+    constructor( private _usersService : UsersService ) {
+        let query   = {
+            expanded    : true
+        };
+
+        this._usersService.query( query )
+            .then( users => this.users = users );
+    }
+}
