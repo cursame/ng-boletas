49aa1cc01ce8bcc68956b47ae8dedd7b7412ef5f
Carlos Cessa <carlos@pixative.com>
2016-07-14 01:57:26 -0500

    Schools Removal
    - Added support to remove a school record from the schools list
    component

--- app/schools/schools-list.component.ts
+++ app/schools/schools-list.component.ts
@@ -6,9 +6,15 @@ import {
 } from '@angular/router';
 
 import {
+    TranslateService
+} from 'ng2-translate/ng2-translate';
+
+import {
     School,
     SchoolsService
 } from './schools.module';
+
+declare var swal : any;
 
 @Component({
     directives  : [ ROUTER_DIRECTIVES ],
@@ -20,7 +26,7 @@ export class SchoolsListComponent {
 
     schools : School[]  = [];
 
-    constructor( private _service : SchoolsService ) {
+    constructor( private _service : SchoolsService, private _translate : TranslateService ) {
         let query   = {
             page        : 1,
             per_page    : 20
@@ -28,5 +34,17 @@ export class SchoolsListComponent {
 
         this._service.query( query )
             .then( schools => this.schools = schools );
+    }
+
+    public remove( id : string, index : number ) {
+        console.log( index );
+        this._service.remove( id )
+            .then( school => {
+                this.schools.splice( index, 1 );
+                swal( this._translate.instant( 'title.schools_removal' ), this._translate.instant( 'message.school_removed' ), 'success' );
+            })
+            .catch( error => {
+                swal( this._translate.instant( 'title.schools_removal' ), this._translate.instant( 'message.school_remove_error' ), 'error' );
+            });
     }
 }
