5ceb2f5f542b78726e958bc2b73bc863b7f30f80
Carlos Cessa <carlos@pixative.com>
2016-07-13 19:43:08 -0500

    Users Service
    - Defined service to interact with application API `users` resource

--- app/users/users.module.ts
+++ app/users/users.module.ts
@@ -1,3 +1,4 @@
 export { User } from './user';
+export { UsersService } from './users.service';
 export { UsersCreateComponent } from './users-create.component';
 export { UsersListComponent } from './users-list.component';
