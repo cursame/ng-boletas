8c6bcf72922559f3927b661996c812cfed3c895e
Carlos Cessa <carlos@pixative.com>
2016-07-15 13:43:49 -0500

    Groups Creation
    - Added support to create a group record in the system
    - Included resources for the new functionality

--- app/groups/groups-create.component.ts
+++ app/groups/groups-create.component.ts
@@ -2,11 +2,17 @@ import {
     Component
 } from '@angular/core';
 import {
+    Router,
     ROUTER_DIRECTIVES
 } from '@angular/router';
 
 import {
-    Group
+    TranslateService
+} from 'ng2-translate/ng2-translate';
+
+import {
+    Group,
+    GroupsService
 } from './groups.module';
 import {
     School,
@@ -17,9 +23,11 @@ import {
     UsersService
 } from '../users/users.module';
 
+declare var swal : any;
+
 @Component({
     directives  : [ ROUTER_DIRECTIVES ],
-    providers   : [ SchoolsService, UsersService ],
+    providers   : [ GroupsService, SchoolsService, UsersService ],
     selector    : 'groups-create',
     templateUrl : 'views/groups/create.html'
 })
@@ -31,7 +39,7 @@ export class GroupsCreateComponent {
 
     users   : User[]    = [];
 
-    constructor( private _schoolsService : SchoolsService, private _usersService : UsersService ) {
+    constructor( private _router : Router, private _service : GroupsService, private _schoolsService : SchoolsService, private _usersService : UsersService, private _translate : TranslateService ) {
         let query   = {
             page        : 1,
             per_page    : 9999
@@ -39,6 +47,17 @@ export class GroupsCreateComponent {
 
         this._schoolsService.query( query )
             .then( schools => this.schools = schools );
+    }
+
+    public create() {
+        this._service.create( this.group )
+            .then( group => {
+                this._router.navigate([ '/groups/list' ]);
+                swal( this._translate.instant( 'title.groups_creation' ), this._translate.instant( 'message.group_created' ), 'success' );
+            })
+            .catch( error => {
+                swal( this._translate.instant( 'title.groups_creation' ), this._translate.instant( 'message.group_create_error' ), 'error' );
+            });
     }
 
     public schoolChanged( school ) {
