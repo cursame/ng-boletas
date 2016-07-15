c1e6043c2edd866fc969d5087746e982e9a754ec
Carlos Cessa <carlos@pixative.com>
2016-07-15 14:44:56 -0500

    Courses Schools
    - Added support to list the available schools in the courses creation
    view

--- app/courses/courses-create.component.ts
+++ app/courses/courses-create.component.ts
@@ -8,13 +8,30 @@ import {
 import {
     Course
 } from './courses.module';
+import {
+    School,
+    SchoolsService
+} from '../schools/schools.module';
 
 @Component({
     directives  : [ ROUTER_DIRECTIVES ],
+    providers   : [ SchoolsService ],
     selector    : 'courses-create',
     templateUrl : 'views/courses/create.html'
 })
 export class CoursesCreateComponent {
 
-    course  : Course    = new Course();
+    course      : Course    = new Course();
+
+    schools     : School[]  = [];
+
+    constructor( private _schoolsService : SchoolsService ) {
+        let query   = {
+            page        : 1,
+            per_page    : 9999
+        };
+
+        this._schoolsService.query( query )
+            .then( schools => this.schools = schools );
+    }
 }
