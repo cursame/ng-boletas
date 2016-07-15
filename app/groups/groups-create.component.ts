481c40a90f3ad6a388b5ba7ae30555c9b818f76d
Carlos Cessa <carlos@pixative.com>
2016-07-15 13:18:46 -0500

    Groups Schools
    - Added support to the groups create component to load a list of
    schools to select when creating a group

--- app/groups/groups-create.component.ts
+++ app/groups/groups-create.component.ts
@@ -8,13 +8,30 @@ import {
 import {
     Group
 } from './groups.module';
+import {
+    School,
+    SchoolsService
+} from '../schools/schools.module';
 
 @Component({
     directives  : [ ROUTER_DIRECTIVES ],
+    providers   : [ SchoolsService ],
     selector    : 'groups-create',
     templateUrl : 'views/groups/create.html'
 })
 export class GroupsCreateComponent {
 
-    group : Group   = new Group();
+    group   : Group     = new Group();
+
+    schools : School[]  = [];
+
+    constructor( private _schoolsService : SchoolsService ) {
+        let query   = {
+            page        : 1,
+            per_page    : 9999
+        };
+
+        this._schoolsService.query( query )
+            .then( schools => this.schools = schools );
+    }
 }
