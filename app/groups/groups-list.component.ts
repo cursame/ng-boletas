7f018dbde1355ff5bccbbe0e8d4ff12c828d5891
Carlos Cessa <carlos@pixative.com>
2016-07-15 13:48:13 -0500

    Groups List
    - Added support in the groups list component to retrieve a list of
    groups from the database

--- app/groups/groups-list.component.ts
+++ app/groups/groups-list.component.ts
@@ -5,9 +5,27 @@ import {
     ROUTER_DIRECTIVES
 } from '@angular/router';
 
+import {
+    Group,
+    GroupsService
+} from './groups.module';
+
 @Component({
     directives  : [ ROUTER_DIRECTIVES ],
+    providers   : [ GroupsService ],
     selector    : 'groups-list',
     templateUrl : 'views/groups/list.html'
 })
-export class GroupsListComponent {}
+export class GroupsListComponent {
+
+    groups  : Group[]   = [];
+
+    constructor( private _service : GroupsService ) {
+        let query   = {
+            expanded    : true
+        };
+
+        this._service.query( query )
+            .then( groups => this.groups = groups );
+    }
+}
