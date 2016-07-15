8189230573e803b85aaafbb864446af5d72fe7fb
Carlos Cessa <carlos@pixative.com>
2016-07-15 13:39:40 -0500

    Groups Service
    - Created service to interact with the application's backend `groups`
    resource

--- app/groups/groups.module.ts
+++ app/groups/groups.module.ts
@@ -1,3 +1,4 @@
 export { Group } from './group';
+export { GroupsService } from './groups.service';
 export { GroupsCreateComponent } from './groups-create.component';
 export { GroupsListComponent } from './groups-list.component';
---
+++ app/groups/groups.service.ts
@@ -0,0 +1,22 @@
+import {
+    Injectable
+} from '@angular/core';
+import {
+    Http
+} from '@angular/http';
+
+import {
+    CommonService
+} from '../common/common.module';
+import {
+    SessionsService
+} from '../sessions/sessions.module';
+
+@Injectable()
+export class GroupsService extends CommonService {
+
+    constructor( protected _http : Http, protected _sessionService : SessionsService ) {
+        super();
+        this._init( 'groups' );
+    }
+}
