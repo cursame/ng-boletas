f5b9929dcb65b60f0a5ed67191e8d44452a0ac96
Carlos Cessa <carlos@pixative.com>
2016-07-18 13:29:16 -0500

    Courses Students
    - Added support to list the available students in the courses creation
    view

--- app/courses/courses-create.component.ts
+++ app/courses/courses-create.component.ts
@@ -16,10 +16,14 @@ import {
     School,
     SchoolsService
 } from '../schools/schools.module';
+import {
+    User,
+    UsersService
+} from '../users/users.module';
 
 @Component({
     directives  : [ ROUTER_DIRECTIVES ],
-    providers   : [ GroupsService, SchoolsService ],
+    providers   : [ GroupsService, SchoolsService, UsersService ],
     selector    : 'courses-create',
     templateUrl : 'views/courses/create.html'
 })
@@ -31,7 +35,9 @@ export class CoursesCreateComponent {
 
     schools     : School[]  = [];
 
-    constructor( private _schoolsService : SchoolsService, private _groupsService : GroupsService ) {
+    students    : User[]    = [];
+
+    constructor( private _schoolsService : SchoolsService, private _groupsService : GroupsService, private _usersService : UsersService ) {
         let query   = {
             page        : 1,
             per_page    : 9999
@@ -42,14 +48,25 @@ export class CoursesCreateComponent {
     }
 
     public schoolChanged( school ) {
-        let groupsQuery = {
+        let groupsQuery     = {
             page        : 1,
             per_page    : 9999,
             school      : school,
             select      : 'name'
         };
 
+        let studentsQuery   = {
+            page        : 1,
+            per_page    : 9999,
+            school      : school,
+            select      : 'name',
+            type        : 4
+        };
+
         this._groupsService.query( groupsQuery )
             .then( groups => this.groups = groups );
+
+        this._usersService.query( studentsQuery )
+            .then( students => this.students = students );
     }
 }
