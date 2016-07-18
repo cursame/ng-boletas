88a6fa555567d7a3f6e105f6492d437beb82e078
Carlos Cessa <carlos@pixative.com>
2016-07-18 13:25:09 -0500

    Courses Groups
    - Added support to list the available groups in the courses creation
    view

--- app/courses/courses-create.component.ts
+++ app/courses/courses-create.component.ts
@@ -9,13 +9,17 @@ import {
     Course
 } from './courses.module';
 import {
+    Group,
+    GroupsService
+} from '../groups/groups.module';
+import {
     School,
     SchoolsService
 } from '../schools/schools.module';
 
 @Component({
     directives  : [ ROUTER_DIRECTIVES ],
-    providers   : [ SchoolsService ],
+    providers   : [ GroupsService, SchoolsService ],
     selector    : 'courses-create',
     templateUrl : 'views/courses/create.html'
 })
@@ -23,9 +27,11 @@ export class CoursesCreateComponent {
 
     course      : Course    = new Course();
 
+    groups      : Group[]   = [];
+
     schools     : School[]  = [];
 
-    constructor( private _schoolsService : SchoolsService ) {
+    constructor( private _schoolsService : SchoolsService, private _groupsService : GroupsService ) {
         let query   = {
             page        : 1,
             per_page    : 9999
@@ -33,5 +39,17 @@ export class CoursesCreateComponent {
 
         this._schoolsService.query( query )
             .then( schools => this.schools = schools );
+    }
+
+    public schoolChanged( school ) {
+        let groupsQuery = {
+            page        : 1,
+            per_page    : 9999,
+            school      : school,
+            select      : 'name'
+        };
+
+        this._groupsService.query( groupsQuery )
+            .then( groups => this.groups = groups );
     }
 }
