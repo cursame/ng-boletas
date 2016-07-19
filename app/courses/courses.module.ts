0f42e8996aaf681e8f6c65ea1afa139a1bcc8d2a
Carlos Cessa <carlos@pixative.com>
2016-07-19 12:58:22 -0500

    Courses Service
    - Added service to interact with the backend's courses resource

--- app/courses/courses.module.ts
+++ app/courses/courses.module.ts
@@ -1,3 +1,4 @@
 export { Course } from './course';
+export { CoursesService } from './courses.service';
 export { CoursesCreateComponent } from './courses-create.component';
 export { CoursesListComponent } from './courses-list.component';
