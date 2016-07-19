e23551e472e9202d92bea57f3efc7aad76767064
Carlos Cessa <carlos@pixative.com>
2016-07-19 13:06:52 -0500

    Courses List
    - Added support to display a list of courses in the application's
    courses list component
    - Included HTML markup for the modified view

--- app/courses/courses-list.component.ts
+++ app/courses/courses-list.component.ts
@@ -1,13 +1,34 @@
 import {
-    Component
+    Component,
+    OnInit
 } from '@angular/core';
 import {
     ROUTER_DIRECTIVES
 } from '@angular/router';
 
+import {
+    Course,
+    CoursesService
+} from './courses.module';
+
 @Component({
     directives  : [ ROUTER_DIRECTIVES ],
+    providers   : [ CoursesService ],
     selector    : 'courses-list',
     templateUrl : 'views/courses/list.html'
 })
-export class CoursesListComponent {}
+export class CoursesListComponent implements OnInit {
+
+    courses     : Course[]  = [];
+
+    constructor( private _service : CoursesService ) {}
+
+    public ngOnInit() {
+        let query   = {
+            expanded    : true
+        };
+
+        this._service.query( query )
+            .then( courses => this.courses = courses );
+    }
+}
