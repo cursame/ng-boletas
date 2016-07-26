46dcc1def85d2ae08eefe775345c5a54c16ded0f
Carlos Cessa <carlos@pixative.com>
2016-07-26 13:25:56 -0500

    Periods List
    - Added support to display a list of periods in the application's
    periods list component

--- app/periods/periods-list.component.ts
+++ app/periods/periods-list.component.ts
@@ -5,9 +5,27 @@ import {
     ROUTER_DIRECTIVES
 } from '@angular/router';
 
+import {
+    Period,
+    PeriodsService
+} from './periods.module';
+
 @Component({
     directives  : [ ROUTER_DIRECTIVES ],
+    providers   : [ PeriodsService ],
     selector    : 'periods-list',
     templateUrl : 'views/periods/list.html'
 })
-export class PeriodsListComponent {}
+export class PeriodsListComponent {
+
+    periods : Period[]  = [];
+
+    constructor( private _periodsService : PeriodsService ) {
+        let query   = {
+            expanded    : true
+        };
+
+        this._periodsService.query( query )
+            .then( periods => this.periods = periods );
+    }
+}
