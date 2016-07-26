b7cc87369c1461efe2468b22f14c32db62d60fd5
Carlos Cessa <carlos@pixative.com>
2016-07-26 13:28:30 -0500

    Periods Removal
    - Added support to remove a period record from the application

--- app/periods/periods-list.component.ts
+++ app/periods/periods-list.component.ts
@@ -6,9 +6,15 @@ import {
 } from '@angular/router';
 
 import {
+    TranslateService
+} from 'ng2-translate/ng2-translate';
+
+import {
     Period,
     PeriodsService
 } from './periods.module';
+
+declare var swal : any;
 
 @Component({
     directives  : [ ROUTER_DIRECTIVES ],
@@ -20,12 +26,23 @@ export class PeriodsListComponent {
 
     periods : Period[]  = [];
 
-    constructor( private _periodsService : PeriodsService ) {
+    constructor( private _periodsService : PeriodsService, private _translate : TranslateService ) {
         let query   = {
             expanded    : true
         };
 
         this._periodsService.query( query )
             .then( periods => this.periods = periods );
+    }
+
+    public remove( id : string, index : number ) {
+        this._periodsService.remove( id )
+            .then( user => {
+                this.periods.splice( index, 1 );
+                swal( this._translate.instant( 'title.periods_removal' ), this._translate.instant( 'message.period_removed' ), 'success' );
+            })
+            .catch( error => {
+                swal( this._translate.instant( 'title.periods_removal' ), this._translate.instant( 'message.period_remove_error' ), 'error' );
+            });
     }
 }
